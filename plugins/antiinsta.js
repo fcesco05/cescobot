f04e47cf0ea1e01c798aa7642db135d4ebdf6aad
ilcescodicosenz <cescokek@gmail.com>
2025-03-03 22:30:49 +0100

    Update antiinsta.js

--- plugins/antiinsta.js
+++ plugins/antiinsta.js
@@ -1,106 +1,113 @@
-function _0x37c8() {
-    const _0x2af448 = [
-        'includes',
-        'sender',
-        'antiinsta',
-        'sendMessage',
-        'user',
-        '1413HccOnC',
-        'groupParticipantsUpdate',
-        '𝐀𝐧𝐭𝐢\x20-\x20𝐈𝐧𝐬𝐭𝐚\x20',
-        'text',
-        'chat',
-        'remove',
-        '728413PxsDvT',
-        'users',
-        'data',
-        'instagram.com',
-        '740718kABKRC',
-        'exec',
-        'warn',
-        '1217196ottOZY',
-        '0@s.whatsapp.net',
-        '32TnmEHW',
-        'Halo',
-        '159690HAElmB',
-        '582khHlyG',
-        'settings',
-        'isBaileys',
-        '⛔\x20𝐔𝐓𝐄𝐍𝐓𝐄\x20𝐑𝐈𝐌𝐎𝐒𝐒𝐎\x20𝐃𝐎𝐏𝐎\x20𝟑\x20𝐀𝐕𝐕𝐄𝐑𝐓𝐈𝐌𝐄𝐍𝐓𝐈',
-        'isGroup',
-        '1640YPEKhU',
-        'key',
-        '*\x20°\x20𝐀𝐕𝐕𝐄𝐑𝐓𝐈𝐌𝐄𝐍𝐓𝐎\x20',
-        'fromMe',
-        'reply',
-        '6274000IvmZhE',
-        'https://telegra.ph/file/e12aae9f5ea6c2e5e52aa.png',
-        '77994wiTrBA',
-        'participant'
-    ];
-    _0x37c8 = function () {
-        return _0x2af448;
-    };
-    return _0x37c8();
-}
+/**
+ * Funzione per gestire il rilevamento dei link di Instagram e l'invio di avvisi o la rimozione dei messaggi.
+ * Questa funzione controlla se in un messaggio di gruppo è stato condiviso un link a Instagram
+ * e, se la funzione anti-instagram è attiva, incrementa il contatore di avvisi dell'utente.
+ * Se l'utente non è amministratore e il bot ha i permessi, elimina il messaggio e invia un avviso.
+ *
+ * @param {Object} message - L'oggetto messaggio ricevuto.
+ * @param {Object} options - Opzioni aggiuntive.
+ * @param {boolean} options.isAdmin - Indica se il mittente è un amministratore.
+ * @param {Object} options.groupMetadata - Metadati del gruppo.
+ * @param {boolean} options.isBotAdmin - Indica se il bot ha i permessi di amministratore.
+ * @returns {boolean} - Ritorna true per continuare il processo del messaggio.
+ */
+export async function before(message, { isAdmin, groupMetadata, isBotAdmin }) {
+  // Se il messaggio è inviato da Baileys (il client) e proviene da se stesso, non fare nulla
+  if (message.isBaileys && message.fromMe) return true;
+  // Se il messaggio non proviene da un gruppo, esci
+  if (!message.isGroup) return false;
 
-function _0x49c5(_0xc453ad, _0x2b38c7) {
-    const _0x37c8ca = _0x37c8();
-    return _0x49c5 = function (_0x49c59a, _0x1f5ff9) {
-        _0x49c59a = _0x49c59a - 0x165;
-        let _0x586718 = _0x37c8ca[_0x49c59a];
-        return _0x586718;
-    }, _0x49c5(_0xc453ad, _0x2b38c7);
-}
+  // Ottieni le impostazioni della chat dal database globale
+  const chatSettings = global.db.data.chats[message.chat];
+  const warningLimit = 5; // Numero massimo di avvisi consentiti
+  const messageKeyId = message.key.id; // ID univoco del messaggio
+  // Impostazioni aggiuntive (se necessarie)
+  const settings = global.db.data.settings[this.user.jid] || {};
 
-(function (_0x2a5c63, _0x466bbd) {
-    const _0x2d72d6 = _0x49c5,
-        _0x2bc1da = _0x2a5c63();
-    while (!![]) {
-        try {
-            const _0x490172 = -parseInt(_0x2d72d6(0x16a)) / 0x1 * (parseInt(_0x2d72d6(0x17c)) / 0x2) + -parseInt(_0x2d72d6(0x174)) / 0x3 + -parseInt(_0x2d72d6(0x179)) / 0x4 * (-parseInt(_0x2d72d6(0x17b)) / 0x5) + parseInt(_0x2d72d6(0x177)) / 0x6 + parseInt(_0x2d72d6(0x170)) / 0x7 + -parseInt(_0x2d72d6(0x186)) / 0x8 + parseInt(_0x2d72d6(0x188)) / 0x9 * (parseInt(_0x2d72d6(0x181)) / 0xa);
-            if (_0x490172 === _0x466bbd) break;
-            else _0x2bc1da['push'](_0x2bc1da['shift']());
-        } catch (_0x12cf4b) {
-            _0x2bc1da['push'](_0x2bc1da['shift']());
-        }
+  // Regex per rilevare i link di Instagram
+  const instagramRegex = /instagram\.com/i;
+  // Verifica se il messaggio contiene un link di Instagram
+  const instagramLinkFound = instagramRegex.exec(message.text);
+  const instagramDomain = "instagram.com";
+
+  // Se l'utente è amministratore e la funzione anti-instagram è attiva,
+  // e il messaggio contiene "instagram.com" (ad esempio per configurare la funzione),
+  // non interveniamo
+  if (isAdmin && chatSettings.antiinsta && message.text.includes(instagramDomain)) return true;
+
+  // Se la funzione anti-instagram è attiva, viene trovato un link di Instagram,
+  // l'utente non è amministratore e il bot ha i permessi amministrativi
+  if (chatSettings.antiinsta && instagramLinkFound && !isAdmin && isBotAdmin) {
+    // Incrementa il contatore di avvisi per l'utente
+    global.db.data.users[message.sender].warn += 1;
+
+    // Elimina il messaggio contenente il link
+    await conn.sendMessage(message.chat, {
+      delete: {
+        remoteJid: message.chat,
+        fromMe: false,
+        id: messageKeyId,
+        participant: message.sender
+      }
+    });
+
+    // Ottieni il numero attuale di avvisi dell'utente
+    let currentWarnings = global.db.data.users[message.sender].warn;
+    let userData = global.db.data.users[message.sender];
+
+    // Se il numero di avvisi è inferiore al limite, invia un messaggio di avviso
+    if (currentWarnings < warningLimit) {
+      // Scarica un'immagine da utilizzare come miniatura per l'avviso
+      const warningImage = await (await fetch('https://telegra.ph/file/e12aae9f5ea6c2e5e52aa.png')).buffer();
+
+      // Crea il contenuto del messaggio di avviso, inclusa una vCard (contatto)
+      let warningContent = {
+        key: { participants: "0@s.whatsapp.net", fromMe: false, id: "Halo" },
+        message: {
+          locationMessage: {
+            name: "𝐀𝐧𝐭𝐢 - 𝐈𝐧𝐬𝐭𝐚",
+            jpegThumbnail: warningImage,
+            vcard:
+              'BEGIN:VCARD\n' +
+              'VERSION:1.0\n' +
+              'N:;Unlimited;;;\n' +
+              'FN:Unlimited\n' +
+              'ORG:Unlimited\n' +
+              'TITLE:\n' +
+              'item1.TEL;waid=19709001746:+1 (970) 900-1746\n' +
+              'item1.X-ABLabel:Unlimited\n' +
+              'X-WA-BIZ-DESCRIPTION:ofc\n' +
+              'X-WA-BIZ-NAME:Unlimited\n' +
+              'END:VCARD'
+          }
+        },
+        participant: "0@s.whatsapp.net"
+      };
+
+      // Invia il messaggio di avviso come risposta nel gruppo
+      conn.reply(
+        message.chat,
+        '⚠ LINK INSTAGRAM NON SONO PERMESSI. FAI .setig PER IMPOSTARE INSTAGRAM\n*' +
+          userData.warn +
+          '* ° 𝐀𝐕𝐕𝐄𝐑𝐓𝐈𝐌𝐄𝐍𝐓𝐎 ',
+        warningContent
+      );
     }
-}(_0x37c8, 0x84282));
+    // (Eventualmente, se si desidera rimuovere l'utente dopo un certo numero di avvisi, si può aggiungere:
+    // else {
+    //   global.db.data.users[message.sender].warn = 0;
+    //   await conn.sendMessage(message.chat, '⛔ AVVISO: Hai superato il limite di avvisi. Azione intrapresa.', {
+    //     delete: {
+    //       remoteJid: message.chat,
+    //       fromMe: false,
+    //       id: messageKeyId,
+    //       participant: message.sender
+    //     }
+    //   });
+    //   await conn.groupParticipantsUpdate(message.chat, [message.sender], 'remove');
+    // }
+    // )
+  }
 
-let linkRegex = /instagram.com/i;
-
-export async function before(_0x216c18, { isAdmin: _0x524171, groupMetadata: _0x3b8efa, isBotAdmin: _0x282857 }) {
-    const _0x43af82 = _0x49c5;
-    if (_0x216c18[_0x43af82(0x17e)] && _0x216c18[_0x43af82(0x184)]) return !0x0;
-    if (!_0x216c18[_0x43af82(0x180)]) return !0x1;
-    let _0x2e6aea = global['db']['data']['chats'][_0x216c18[_0x43af82(0x16e)]],
-        _0x3ec60 = '5',
-        _0x4bfee6 = _0x216c18[_0x43af82(0x182)][_0x43af82(0x189)],
-        _0x3b2aef = _0x216c18[_0x43af82(0x182)]['id'],
-        _0x3c1f69 = global['db'][_0x43af82(0x172)][_0x43af82(0x17d)][this[_0x43af82(0x169)]['jid']] || {};
-    const _0xd293a0 = linkRegex[_0x43af82(0x175)](_0x216c18[_0x43af82(0x16d)]),
-        _0x318b8a = _0x43af82(0x173);
-    if (_0x524171 && _0x2e6aea['antiinsta'] && _0x216c18['text'][_0x43af82(0x165)](_0x318b8a)) return;
-    if (_0x2e6aea[_0x43af82(0x167)] && _0xd293a0 && !_0x524171 && _0x282857) {
-        if (_0x282857) {
-            global['db'][_0x43af82(0x172)][_0x43af82(0x171)][_0x216c18['sender']]['warn'] += 0x1, await conn[_0x43af82(0x168)](_0x216c18[_0x43af82(0x16e)], { 'delete': { 'remoteJid': _0x216c18[_0x43af82(0x16e)], 'fromMe': ![], 'id': _0x3b2aef, 'participant': _0x4bfee6 } });
-            let _0x286443 = global['db'][_0x43af82(0x172)][_0x43af82(0x171)][_0x216c18[_0x43af82(0x166)]][_0x43af82(0x176)],
-                _0x53f0c7 = global['db'][_0x43af82(0x172)][_0x43af82(0x171)][_0x216c18[_0x43af82(0x166)]];
-            if (_0x286443 < _0x3ec60) {
-                let _0xe36ac7 = {
-                    'key': { 'participants': _0x43af82(0x178), 'fromMe': ![], 'id': _0x43af82(0x17a) },
-                    'message': {
-                        'locationMessage': {
-                            'name': _0x43af82(0x16c),
-                            'jpegThumbnail': await (await fetch(_0x43af82(0x187)))['buffer'](),
-                            'vcard': 'BEGIN:VCARD\x0aVERSION:1.0\x0aN:;Unlimited;;;\x0aFN:Unlimited\x0aORG:Unlimited\x0aTITLE:\x0aitem1.TEL;waid=19709001746:+1\x20(970)\x20900-1746\x0aitem1.X-ABLabel:Unlimited\x0aX-WA-BIZ-DESCRIPTION:ofc\x0aX-WA-BIZ-NAME:Unlimited\x0aEND:VCARD'
-                        }
-                    },
-                    'participant': _0x43af82(0x178)
-                };
-                conn[_0x43af82(0x185)](_0x216c18[_0x43af82(0x16e)], '⚠\x20𝐋𝐈𝐍𝐊\x20𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌\x20𝐍𝐎𝐍\x20𝐒𝐎𝐍𝐎\x20𝐂𝐎𝐍𝐒𝐄𝐍𝐓𝐈𝐓𝐈\x20\x0a*' + _0x53f0c7[_0x43af82(0x176)] + _0x43af82(0x183), _0xe36ac7);
-            } else (_0x459432 => _0x3ec60) && (global['db'][_0x43af82(0x172)][_0x43af82(0x171)][_0x216c18['sender']]['warn'] = 0x0, _0x216c18[_0x43af82(0x185)](_0x43af82(0x17f)), await conn[_0x43af82(0x16b)](_0x216c18[_0x43af82(0x16e)], [_0x216c18['sender']], _0x43af82(0x16f)));
-        }
-    }
-    return !0x0;
+  return true;
 }
