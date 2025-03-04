4d42118a5b4ceffefc27d6ad059457d257ccb640
ilcescodicosenz <cescokek@gmail.com>
2025-03-05 00:33:28 +0100

    Update info-owner.js

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,56 +1,24 @@
-(function(array, valore) {
-  const funzione = ottieniParola;
-  const arr = array();
-  while (!![]) {
-    try {
-      const risultato = -parseInt(funzione(0x127)) / 0x1 + -parseInt(funzione(0x130)) / 0x2 * (parseInt(funzione(0x12f)) / 0x3) + parseInt(funzione(0x11c)) / 0x4 * (parseInt(funzione(0x11a)) / 0x5) + -parseInt(funzione(0x129)) / 0x6 + parseInt(funzione(0x128)) / 0x7 * (parseInt(funzione(0x12d)) / 0x8) + -parseInt(funzione(0x11f)) / 0x9 * (parseInt(funzione(0x11e)) / 0xa) + parseInt(funzione(0x120)) / 0xb;
-      if (risultato === valore) break;
-      else arr.push(arr.shift());
-    } catch (errore) {
-      arr.push(arr.shift());
-    }
-  }
-})(ottieniArrayStringhe, 0x51488);
-
-function gestore(messaggio) {
-  const ottieni = ottieniParola;
-  let infoContatto = {
-    'key': {
-      'partecipanti': ottieni(0x12a),
-      'daMe': false,
-      'id': ottieni(0x122)
+function handler(m) {
+  const contactInfo = {
+    key: {
+      participants: 'owner@s.whatsapp.net',  // ID dell'owner
+      fromMe: false,  // Indica che il messaggio non proviene dal bot
+      id: 'some_random_id',  // ID del messaggio
     },
-    'message': {
-      'extendedTextMessage': {
-        'text': ottieni(0x119),
-        'vcard': ottieni(0x118)
-      }
+    message: {
+      extendedTextMessage: {
+        text: 'ei come posso aiutarti?',  // Messaggio di risposta
+        vcard: 'BEGIN:VCARD\nVERSION:1.0\nN:;Unlimited;;;\nFN:Unlimited\nORG:Unlimited\nTITLE:\nitem1.TEL;waid=19709001746:+1 (970) 900-1746\nitem1.X-ABLabel:Unlimited\nX-WA-BIZ-DESCRIPTION:ofc\nX-WA-BIZ-NAME:Unlimited\nEND:VCARD'  // Contatto in formato vCard
+      },
     },
-    'participant': ottieni(0x12a)
+    participant: 'owner@s.whatsapp.net',  // ID dell'owner
   };
-  const globaleFiltrato = global['tags']['filter'](([chiave, valore]) => chiave && valore);
-  this['inviaContatto'](messaggio['chat'], globaleFiltrato['map'](([chiave, valore]) => [chiave, valore]), infoContatto);
+
+  const participants = global.plugins.filter(([key, value]) => key && value);
+  this.sendMessage(m.chat, participants.map(([key, value]) => [key, value]), contactInfo);
 }
 
-gestore['principale'] = ['tags'];
-gestore['aiuto'] = ['chat'];
-gestore['comando'] = ['proprietario', 'creador', 'dueño', 'fgowner'];
+handler.tags = ['owner'];
+handler.command = ['proprietario', 'help', 'owner', 'fgowner'];
 
-function ottieniArrayStringhe() {
-  const arrayStringhe = ['2403618kkIOrP', '0@s.whatsapp.net', 'help', 'chat', '6696iPmJHW', 'tags', '3zdkLgp', '452024pPdPzc', 'BEGIN:VCARD\x0aVERSION:1.0\x0aN:;Unlimited;;;\x0aFN:Unlimited\x0aORG:Unlimited\x0aTITLE:\x0aitem1.TEL;waid=19709001746:+1\x20(970)\x20900-1746\x0aitem1.X-ABLabel:Unlimited\x0aX-WA-BIZ-DESCRIPTION:ofc\x0aX-WA-BIZ-NAME:Unlimited\x0aEND:VCARD', '𝐎𝐰𝐧𝐞𝐫\x20𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲-𝐁𝐨𝐭', '27085DXPJcB', 'main', '440ADyhZU', 'owner', '1790CNRxOe', '8739PPFDwZ', '7873184bSpQbi', 'sendContact', 'Halo', 'creador', 'filter', 'map', 'dueño', '619353jwuibX', '3689kioSZg'];
-  ottieniArrayStringhe = function() {
-    return arrayStringhe;
-  };
-  return ottieniArrayStringhe();
-}
-
-function ottieniParola(indice, fittizio) {
-  const arr = ottieniArrayStringhe();
-  return ottieniParola = function(indice, fittizio) {
-    indice = indice - 0x118;
-    let valore = arr[indice];
-    return valore;
-  }, ottieniParola(indice, fittizio);
-}
-
-export default gestore;
+export default handler;
