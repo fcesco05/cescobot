562febe1f4ad6c7c1fef031ae51a4ae2f20dc782
ilcescodicosenz <cescokek@gmail.com>
2025-03-06 22:41:07 +0100

    Update ai-chatgpt.js

--- plugins/ai-chatgpt.js
+++ plugins/ai-chatgpt.js
@@ -35,7 +35,33 @@ try {
 let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/gptweb?text=${text}`) 
 let res = await gpt.json()
 await m.reply(res.gpt)
-/*let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/chatgpt?q=${text}`)
+/*let gpt = await fetch(`https://deliriusapi-official.vercel.app/ia/chatgpt?q=${text}`)import fetch from 'node-fetch';
+
+var handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) {
+        await m.reply(`Per favore, inserisci una domanda per far sì che Gemini risponda.\n\nEsempio:\n${usedPrefix + command} Raccomanda un top 10 di film d'azione\n${usedPrefix + command} Codice in JS per un gioco di carte`);
+        return;
+    }
+
+    try {
+        conn.sendPresenceUpdate('composing', m.chat);
+        var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${text}`);
+        var res = await apii.json();
+        await m.reply(res.result);
+    } catch (e) {
+        await conn.reply(m.chat, `Si è verificato un errore. Per favore, riprova più tardi.\n\n#report ${usedPrefix + command}\n\n${wm}`, fkontak, m);
+        console.log(`Errore nel comando ${usedPrefix + command}`);
+        console.log(e);
+    }
+};
+
+handler.command = ['bard', 'gemini', 'ia'];
+handler.help = ['bard', 'gemini'];
+handler.tags = ['herramientas'];
+
+handler.premium = false;
+
+export default handler;
 let res = await gpt.json()
 await m.reply(res.data)*/
 } catch {
